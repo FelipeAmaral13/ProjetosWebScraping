00f02a3f10e9f5a21a97f12bc1b57241b0198e2e
FelipeMeganha <felipengmec@gmail.com>
2020-08-07 09:40:33 -0300

    tripadvisor scraping

--- EstudoTripAdvisor/tripadvisor/tripadvisor/items.py
+++ EstudoTripAdvisor/tripadvisor/tripadvisor/items.py
@@ -10,3 +10,7 @@ class TripadvisorItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     autor_comentario = scrapy.Field()
+    autor_endereco = scrapy.Field()
+    comentario_titulo = scrapy.Field()
+    comentario_corpo = scrapy.Field()
+    comentario_data = scrapy.Field()
--- EstudoTripAdvisor/tripadvisor/tripadvisor/spiders/comentarios.py
+++ EstudoTripAdvisor/tripadvisor/tripadvisor/spiders/comentarios.py
@@ -1,5 +1,5 @@
 import scrapy
-from ..items import TripadvisorItem
+from tripadvisor.items import TripadvisorItem
 
 class ComentariosSpider(scrapy.Spider):
     name = 'comentarios'
@@ -9,9 +9,19 @@ class ComentariosSpider(scrapy.Spider):
     def parse(self, response):
         item = TripadvisorItem()
         quadros_de_comentarios = response.xpath("//div[@class='Dq9MAugU T870kzTX LnVzGwUB']")
+        
         for quadro in quadros_de_comentarios:
             item['autor_comentario'] = quadro.xpath(".//span/a[@class='ui_header_link _1r_My98y']/text()").get()
+            item['autor_endereco'] = quadro.xpath('.//span[@class="default _3J15flPT small"]/text()').get()
+            item['comentario_titulo'] = quadro.xpath('.//div[@class="glasR4aX"]/a//span/text()').get()
+            item['comentario_corpo'] = quadro.xpath('.//q[@class="IRsGHoPm"]/span/text()').get()
+            item['comentario_data'] = quadro.xpath('.//span[@class="_34Xs-BQm"]/text()').get()
+
             yield item
 
+        next_page = response.xpath('//a[@class="ui_button nav next primary " and text() = "Próximas"]/@href').get()
+        if next_page:
+            yield response.follow(url=next_page, callback = self.parse)    
 
-        pass
+
+        
